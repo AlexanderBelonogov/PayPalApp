10105458c8b626e1c9a9f3e3db96b14c7ef60a38
Roman Zuboley <roman@artistsoft.com>
2017-10-02 15:44:30 +0300

    Pages without index page and popaps

--- app.rb
+++ app.rb
@@ -8,6 +8,26 @@ class PayPallApp < Sinatra::Base
   get '/' do
     haml :index
   end
+
+  get '/works' do
+    @title = 'Why It Works?'
+    haml :works
+  end
+
+  get '/help' do
+    @title = 'Help'
+    haml :help
+  end
+
+  get '/contact' do
+    @title = 'Contact'
+    haml :contact
+  end
+
+  get '/rules' do
+    @title = 'Terms of Service'
+    haml :rules
+  end
 end
 
 if __FILE__ == $0
